5c33b883d2bfee9bdce7f3e67d709d0580c00356
veryboringhwl <d.linshengjie@gmail.com>
2024-04-29 15:38:07 +0100

    Added (un)installation script

--- README.md
+++ README.md
@@ -7,29 +7,39 @@
 
 ### Fluent
 
-![(image)](https://raw.githubusercontent.com/veryboringhwl/veryboring-theme/cf91f44dd253f807b755f00ef1c40c064befa2e3/assets/placeholder%201.png)
-placeholder
+![(image)](https://raw.githubusercontent.com/veryboringhwl/veryboring-theme/cf91f44dd253f807b755f00ef1c40c064befa2e3/assets/placeholder%202.png)
+
 ### Dark
 
-![(image)](https://raw.githubusercontent.com/veryboringhwl/veryboring-theme/cf91f44dd253f807b755f00ef1c40c064befa2e3/assets/placeholder%201.png)placeholder
+![(image)](https://raw.githubusercontent.com/veryboringhwl/veryboring-theme/cf91f44dd253f807b755f00ef1c40c064befa2e3/assets/placeholder%201.png)
 ## Installation
 
 ### Automatic
 Windows → PowerShell:
 
-```Script```
-### Manual
+```powershell
+iwr -useb https://raw.githubusercontent.com/veryboringhwl/veryboring-theme/main/install.ps1 | iex
+```
+### Manual installation
 1. Clone the repo
 2. Navigate to ```%appdata%\spicetify\themes```
 3. Move `src` folder into `themes`
 4. Rename `src` to `veryboring theme`
 4. Open Powershell and run these commands to apply
 
-```
+```powershell
  spicetify config current_theme "veryboring theme"
  spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
  spicetify apply
 ```
+### Manual Uninstallation
+
+```powershell
+spicetify config color_scheme ''
+spicetify config current_theme marketplace
+spicetify apply
+```
+
 ## Colour scheme (Manual install only)
 The colour schemes are `dark`,`fluent` Apply using 
 
---
+++ install.ps1
@@ -0,0 +1,69 @@
+# Edited from the Comfy spicetify theme script (https://github.com/Comfy-Themes/Spicetify/blob/main/install.ps1)
+param (
+  [string] $version
+)
+
+$PSMinVersion = 3
+
+if ($v) {
+  $version = $v
+}
+
+# Helper functions for pretty terminal output.
+function Write-Part ([string] $Text) {
+  Write-Host $Text -NoNewline
+}
+
+function Write-Emphasized ([string] $Text) {
+  Write-Host $Text -NoNewLine -ForegroundColor "Cyan"
+}
+
+function Write-Done {
+  Write-Host " > " -NoNewline
+  Write-Host "OK" -ForegroundColor "Green"
+}
+
+if ($PSVersionTable.PSVersion.Major -gt $PSMinVersion) {
+  $ErrorActionPreference = "Stop"
+
+  # Enable TLS 1.2 since it is required for connections to GitHub.
+  [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+  $checkSpice = Get-Command spicetify -ErrorAction Silent
+  if ($null -eq $checkSpice) {
+    Write-Host -ForegroundColor Red "Spicetify not found"
+    Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.ps1" | Invoke-Expression
+  }
+
+  # Check ~\.spicetify\Themes directory already exists
+  $spicePath = spicetify -c | Split-Path
+  $sp_dot_dir = "$spicePath\Themes"
+  Write-Part "MAKING FOLDER  "; Write-Emphasized "$sp_dot_dir\test"
+  Remove-Item -Recurse -Force "$sp_dot_dir\test" -ErrorAction Ignore
+  New-Item -Path "$sp_dot_dir\test" -ItemType Directory | Out-Null
+  Write-Done
+
+  # Clone to .spicetify.
+  Write-Part "DOWNLOADING    "; Write-Emphasized $sp_dot_dir
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/veryboringhwl/veryboring-theme/main/src/color.ini" -UseBasicParsing -OutFile "$sp_dot_dir\test\color.ini"
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/veryboringhwl/veryboring-theme/main/src/user.css" -UseBasicParsing -OutFile "$sp_dot_dir\test\user.css"
+  Invoke-WebRequest -Uri "https://raw.githubusercontent.com/veryboringhwl/veryboring-theme/main/src/theme.js" -UseBasicParsing -OutFile "$sp_dot_dir\test\theme.js"
+  Write-Done
+
+  # Installing.
+  Write-Part "INSTALLING `r`n"
+  spicetify config inject_css 1 replace_colors 1 overwrite_assets 1 inject_theme_js 1
+  spicetify config current_theme test
+  Write-Done	
+  
+  # applying.
+  Write-Part "APPLYING";
+  spicetify backup
+  spicetify apply
+  Write-Done
+}
+else {
+  Write-Part "`nYour Powershell version is less than "; Write-Emphasized "$PSMinVersion";
+  Write-Part "`nPlease, update your Powershell downloading the "; Write-Emphasized "'Windows Management Framework'"; Write-Part " greater than "; Write-Emphasized "$PSMinVersion"
+}
+Read-host -prompt "Press any button to exit..."
