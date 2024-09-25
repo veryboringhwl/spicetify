928162cc005f970f1c04b7a112e9fe131893b4f5
veryboringhwl <d.linshengjie@gmail.com>
2024-09-25 20:47:44 +0100

    Fix SpotifyMode + Prepare for React rewrite of theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,11 @@
-(function theme() {
-	if (!(Spicetify.Player.data && Spicetify.Platform)) {
+(async function theme() {
+	if (
+		!(
+			Spicetify.Platform &&
+			Spicetify.React &&
+			Spicetify.ReactDOM
+		)
+	) {
 		setTimeout(theme, 100);
 		return;
 	}
@@ -260,16 +266,6 @@
 	MARK: DYNAMIC LYRICS BACKGROUND
 	*/
 
-	const updateSong = () => {
-		const img = Spicetify.Player.data.item.metadata.image_xlarge_url.replace(
-			"spotify:image:",
-			"https://i.scdn.co/image/"
-		);
-		document.documentElement.style.setProperty("--image", `url("${img}")`);
-	};
-	Spicetify.Player.addEventListener("songchange", updateSong);
-	updateSong();
-
 	waitForElements([".main-yourLibraryX-navItem"], () => {
 		const navItems = document.querySelectorAll(
 			".main-yourLibraryX-navItem .main-yourLibraryX-navLink"
@@ -360,10 +356,14 @@
 		});
 	};
 
+	/*
+	MARK: OPTIONS
+	*/
+
 	const options = [
 		{
 			type: "input",
-			category: "test",
+			category: "Test",
 			name: "test-input",
 			desc: "Does something",
 			defaultValue: 1,
@@ -407,7 +407,7 @@
 		},
 		{
 			type: "toggle",
-			category: "Snippets",
+			category: "trash",
 			name: "hidenowplayview",
 			desc: "Hide cover art in now playing bar",
 			defaultValue: false,
@@ -422,29 +422,29 @@
 		},
 		{
 			type: "toggle",
-			category: "Snippets",
+			category: "ea",
 			name: "transplayicon",
 			desc: "Transparent play/pause button in now playing bar",
 			defaultValue: true,
 		},
 		{
 			type: "toggle",
-			category: "Snippets",
+			category: "ea",
 			name: "npvlargerlyrics",
 			desc: "Now playing view (Right sidebar) has lyrics only",
 			defaultValue: false,
 		},
 		{
 			type: "toggle",
-			category: "Snippets",
+			category: "ea",
 			name: "navlinkaccent",
 			desc: "Coloured fluent selector in navlink",
 			defaultValue: true,
-			tippy: 'Not compatible with "Not compatible with horizontalnav"',
+			tippy: 'Not compatible with "horizontalnav"',
 		},
 		{
 			type: "toggle",
-			category: "Snippets",
+			category: "Misc",
 			name: "highlightnav",
 			desc: "Highlights navlink background on hover/active",
 			defaultValue: true,
@@ -452,14 +452,14 @@
 		},
 		{
 			type: "toggle",
-			category: "Snippets",
+			category: "Misc",
 			name: "homeheader",
 			desc: "Removes coloured gradient from the home page header",
 			defaultValue: true,
 		},
 		{
 			type: "dropdown",
-			category: "test",
+			category: "Test",
 			name: "ea",
 			desc: "Description",
 			defaultValue: "test1",
@@ -472,7 +472,7 @@
 		},
 		{
 			type: "dropdown",
-			category: "test",
+			category: "Test",
 			name: "change-Spotify-mode",
 			desc: "Changes Spotify Mode to either Normal, Developer or Employee",
 			defaultValue: "Both",
@@ -481,7 +481,7 @@
 				{ value: "Normal", label: "Normal" },
 				{ value: "Developer", label: "Developer" },
 				{ value: "Employee", label: "Employee" },
-				{ value: "Both", label: "Both (Dev + Empl)" },
+				{ value: "Both", label: "Dev + Empl" },
 			],
 			run: (value) => {
 				changeSpotifyMode(value);
@@ -537,28 +537,29 @@
 		const pairs = modePairs[mode] || modePairs.Normal;
 
 		const setMode = async (key, value) => {
-			await productState.putValues({ pairs: { [key]: value } });
-			return productState.subValues({ keys: [key] }, () => {
-				productState.putValues({ pairs: { [key]: value } });
+			await Spicetify.Platform.UserAPI._product_state_service.putValues({ pairs: { [key]: value } });
+
+			return Spicetify.Platform.UserAPI._product_state_service.subValues({ keys: [key] }, async (newValues) => {
+				if (newValues[key] !== value) {
+					await Spicetify.Platform.UserAPI._product_state_service.putValues({ pairs: { [key]: value } });
+				}
 			});
 		};
-		if (appDevListener) {
-			appDevListener.cancel();
+		if (window.appDevListener) {
+			await window.appDevListener.cancel();
 		}
-		if (employeeListener) {
-			employeeListener.cancel();
+		if (window.employeeListener) {
+			await window.employeeListener.cancel();
 		}
-		appDevListener = await setMode("app-developer", pairs["app-developer"]);
-		employeeListener = await setMode("employee", pairs["employee"]);
-		const currentState = await productState.getValues();
-		console.log("Current Spotify mode state:", currentState);
+		window.appDevListener = await setMode("app-developer", pairs["app-developer"]);
+		window.employeeListener = await setMode("employee", pairs["employee"]);
 	}
 
 	/*
 	MARK: TOPBAR BUTTON
 	*/
 
-	const settingsIcon = `<svg viewBox="0 0 16 16" width="16px" height="16px" fill="currentcolor"><path d="M8.262 5.72A2.542 2.542 90 108.262 10.804 2.542 2.542 90 108.262 5.72ZM6.991 8.262A1.271 1.271 90 119.533 8.262 1.271 1.271 90 116.991 8.262ZM11.59 3.684A.902.902 90 0110.543 2.962L10.212 1.162A.45.45 0 009.863.804 7.715 7.715 90 006.661.804.45.45 0 006.312 1.162L5.983 2.962A.89.89 0 015.945 3.103.902.902 90 014.793 3.646L3.064 3.03A.45.45 0 002.578 3.152 7.61 7.61 0 00.975 5.914C.919 6.087.972 6.276 1.112 6.394L2.513 7.579C2.55 7.611 2.585 7.645 2.616 7.682 2.938 8.06 2.892 8.625 2.512 8.947L1.111 10.132A.45.45 0 00.974 10.611C1.307 11.635 1.855 12.576 2.578 13.373A.45.45 0 003.064 13.495L4.793 12.88A.915.915 90 014.934 12.842.902.902 90 015.981 13.564L6.31 15.363A.45.45 0 006.66 15.721 7.73 7.73 0 008.261 15.888 7.715 7.715 90 009.861 15.721.45.45 0 0010.21 15.363L10.541 13.563A.89.89 0 0110.579 13.422.902.902 90 0111.731 12.879L13.458 13.494A.45.45 0 0013.944 13.372 7.61 7.61 0 0015.548 10.61C15.604 10.437 15.55 10.248 15.411 10.131L14.01 8.946A.89.89 90 0113.906 8.843C13.584 8.466 13.63 7.9 14.01 7.578L15.411 6.393A.45.45 0 0015.548 5.913C15.215 4.889 14.667 3.948 13.944 3.151A.45.45 0 0013.458 3.029L11.729 3.644A.902.902 90 0111.588 3.682ZM3.177 4.42 4.368 4.843A2.173 2.173 90 007.141 3.533C7.18 3.422 7.212 3.308 7.232 3.191L7.457 1.958A6.49 6.49 0 018.262 1.908 6.47 6.47 0 019.065 1.958L9.291 3.191A2.173 2.173 90 0011.816 4.934C11.932 4.912 12.046 4.883 12.157 4.842L13.348 4.419A6.342 6.342 90 0114.149 5.796L13.189 6.607C12.273 7.381 12.16 8.752 12.939 9.666A2.161 2.161 90 0013.189 9.915L14.147 10.726A6.342 6.342 90 0113.346 12.103L12.155 11.678A2.173 2.173 90 009.382 12.988 2.08 2.08 0 009.291 13.33L9.065 14.563A6.482 6.482 90 018.262 14.613 6.5 6.5 0 017.458 14.563L7.233 13.33A2.173 2.173 90 004.708 11.587C4.592 11.609 4.478 11.638 4.367 11.678L3.176 12.101A6.32 6.32 0 012.375 10.725L3.333 9.914C4.249 9.14 4.363 7.769 3.583 6.855 3.507 6.765 3.423 6.682 3.333 6.606L2.375 5.795A6.29 6.29 0 013.176 4.419Z"/></svg>`;
+	const settingsIcon = `<svg viewBox="0 0 16 16" width="16px" height="16px" fill="currentcolor"><path d="M8.04 5.36a2.68 2.68 90 100 5.36 2.68 2.68 90 000-5.36zM6.7 8.04a1.34 1.34 90 112.68 0 1.34 1.34 90 01-2.68 0zM11.5481 3.2133a.9487.9487 90 01-1.1042-.7598l-.3484-1.8974a.4744.4744 90 00-.3685-.3779 8.1365 8.1365 90 00-3.3755 0 .4744.4744 90 00-.3685.3779l-.3471 1.8974a.9514.9514 90 01-1.2542.7209l-1.8211-.6486a.4757.4757 90 00-.5119.1273c-.7625.8402-1.34 1.8318-1.6911 2.9118a.4717.4717 90 00.1447.5065l1.4767 1.2475a.9434.9434 90 010 1.4418l-1.4767 1.2475a.4717.4717 90 00-.1447.5065A8.0212 8.0212 90 002.0502 13.4268a.4757.4757 90 00.5119.1273l1.8224-.6486a.9487.9487 90 011.2542.7209l.3457 1.8974c.0348.1876.1809.3377.3685.3765a8.1405 8.1405 90 003.3768 0 .473.473 90 00.3672-.3765l.3484-1.8974a.9514.9514 90 011.2542-.7209l1.8211.6486c.1809.0643.3832.0134.5119-.1273.7625-.8402 1.34-1.8318 1.6911-2.9118a.4717.4717 90 00-.1447-.5065l-1.4767-1.2475a.942.942 90 010-1.4418l1.4767-1.2475a.4717.4717 90 00.1447-.5065A8.0199 8.0199 90 0014.0298 2.6532a.4757.4757 90 00-.5119-.1273l-1.8224.6486a.9514.9514 90 01-.1487.0402zm-8.8708.7772 1.2556.4462a2.2914 2.2914 90 003.0217-1.742l.2372-1.2998a6.8407 6.8407 90 011.6951 0l.2385 1.2998a2.2887 2.2887 90 003.0217 1.742L13.4 3.9892c.3417.4476.6258.9353.8442 1.4526l-1.0104.8536a2.2834 2.2834 90 000 3.4894l1.0117.8536a6.6866 6.6866 90 01-.8442 1.4526l-1.2556-.4476a2.2914 2.2914 90 00-3.0217 1.742l-.2385 1.2998a6.8327 6.8327 90 01-1.6951 0l-.2372-1.2998a2.2887 2.2887 90 00-3.0217-1.742L2.68 12.0908a6.6826 6.6826 90 01-.8442-1.4526l1.0104-.8549a2.2834 2.2834 90 000-3.488l-1.0117-.8536c.2198-.5172.5038-1.005.8442-1.4526z"/></svg>`;
 
 	new Spicetify.Topbar.Button(
 		"Theme Settings",
