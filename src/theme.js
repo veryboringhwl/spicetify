6fa5d4944ddeac83521ecc9110784155cd245204
veryboringhwl <d.linshengjie@gmail.com>
2024-10-04 21:23:15 +0100

    Fix Spotify mode + use react for github button

--- src/theme.js
+++ src/theme.js
@@ -158,75 +158,17 @@
 		updateCSS(options);
 	};
 
-	const ConfirmDialog = Spicetify.React.memo(({ titleText, descriptionText, cancelText, confirmText, onOpen, onClose, onConfirm }) => {
-		const [isOpen, setIsOpen] = Spicetify.React.useState(true);
-		const self = document.querySelector('.Spicetify.ReactModalPortal:last-of-type');
-		const { ConfirmDialog: SpicetifyConfirmDialog } = Spicetify.ReactComponent;
-
-		const handleClose = () => {
-			setIsOpen(false);
-			onClose?.();
-			self.remove();
-		};
-
-		const handleConfirm = () => {
-			setIsOpen(false);
-			onConfirm?.();
-			self.remove();
-		};
-
-		Spicetify.React.useEffect(() => {
-			if (isOpen) onOpen?.();
-		}, [isOpen, onOpen]);
-
-		const commonProps = {
-			titleText,
-			descriptionText,
-			cancelText,
-			confirmText,
-			isOpen,
-			onClose: handleClose,
-			onConfirm: handleConfirm,
-		};
-
-		return typeof SpicetifyConfirmDialog === 'function'
-			? SpicetifyConfirmDialog(commonProps)
-			: Spicetify.React.createElement(SpicetifyConfirmDialog, commonProps);
-	});
-
 	const resetOptions = (setSettings) => {
-		const settings = document.querySelector('.GenericModal__overlay');
-		Spicetify.ReactDOM.render(
-			Spicetify.React.createElement(
-				Spicetify.ReactComponent.RemoteConfigProvider,
-				{ configuration: Spicetify.Platform.RemoteConfiguration },
-				Spicetify.React.createElement(ConfirmDialog, {
-					titleText: "Are you sure?",
-					descriptionText: "This will reset all settings to default!",
-					cancelText: "Cancel",
-					confirmText: "Reset",
-					onOpen: () => {
-						settings.style.zIndex = 0;
-					},
-					onClose: () => {
-						settings.style.zIndex = 100;
-					},
-					onConfirm: () => {
-						console.log('Theme: Restored to default');
-						const defaultSettings = {};
-						options.forEach((option) => {
-							const prefixedName = `theme:${option.name}`;
-							defaultSettings[prefixedName] = option.defaultValue;
-							localStorage.setItem(prefixedName, JSON.stringify(option.defaultValue));
-							option.run?.(option.defaultValue);
-						});
-						setSettings(defaultSettings);
-						updateCSS(options);
-					},
-				})
-			),
-			document.createElement('div')
-		);
+		console.log('Theme: Restored to default');
+		const defaultSettings = {};
+		options.forEach((option) => {
+			const prefixedName = `theme:${option.name}`;
+			defaultSettings[prefixedName] = option.defaultValue;
+			localStorage.setItem(prefixedName, JSON.stringify(option.defaultValue));
+			option.run?.(option.defaultValue);
+		});
+		setSettings(defaultSettings);
+		updateCSS(options);
 	};
 
 	const CategoryCarousel = Spicetify.React.memo(({ categories, onCategoryClick }) => {
@@ -238,7 +180,7 @@
 					carouselRef.current.scrollLeft += e.deltaY;
 					e.preventDefault();
 				}
-	};
+			};
 
 			const carousel = carouselRef.current;
 			if (carousel) {
@@ -288,8 +230,8 @@
 		}, [prefixedName, onSettingChange]);
 
 		const renderControl = () => {
-		switch (type) {
-			case "toggle":
+			switch (type) {
+				case "toggle":
 					return Spicetify.React.createElement(
 						"button",
 						{
@@ -304,7 +246,7 @@
 							})
 						)
 					);
-			case "dropdown":
+				case "dropdown":
 					return Spicetify.React.createElement(
 						"select",
 						{
@@ -316,7 +258,7 @@
 							Spicetify.React.createElement("option", { key: value, value }, label)
 						)
 					);
-			case "input":
+				case "input":
 					return Spicetify.React.createElement("input", {
 						className: "themeOptionInput",
 						type: "text",
@@ -344,7 +286,7 @@
 			),
 			Spicetify.React.createElement("div", { className: "themeOptionControl" }, renderControl())
 		);
-		});
+	});
 
 	const CategorySection = Spicetify.React.memo(({ category, options, settings, onSettingChange }) => {
 		const [isExpanded, setIsExpanded] = Spicetify.React.useState(true);
@@ -583,9 +525,7 @@
 	MARK: FUNCTIONS
 	*/
 
-	let appDevListener, employeeListener;
 	async function changeSpotifyMode(mode) {
-		const productState = Spicetify.Platform.UserAPI._product_state_service;
 		const modePairs = {
 			Normal: { "app-developer": "0", employee: "0" },
 			Developer: { "app-developer": "2", employee: "0" },
@@ -593,23 +533,23 @@
 			Both: { "app-developer": "2", employee: "1" },
 		};
 		const pairs = modePairs[mode] || modePairs.Normal;
-
 		const setMode = async (key, value) => {
-			await productState.putValues({ pairs: { [key]: value } });
-			return productState.subValues({ keys: [key] }, () => {
-				productState.putValues({ pairs: { [key]: value } });
+			await Spicetify.Platform.UserAPI._product_state_service.putValues({ pairs: { [key]: value } });
+
+			return Spicetify.Platform.UserAPI._product_state_service.subValues({ keys: [key] }, async (newValues) => {
+				if (newValues[key] !== value) {
+					await Spicetify.Platform.UserAPI._product_state_service.putValues({ pairs: { [key]: value } });
+				}
 			});
 		};
-		if (appDevListener) {
-			appDevListener.cancel();
+		if (window.appDevListener) {
+			await window.appDevListener.cancel();
 		}
-		if (employeeListener) {
-			employeeListener.cancel();
+		if (window.employeeListener) {
+			await window.employeeListener.cancel();
 		}
-		appDevListener = await setMode("app-developer", pairs["app-developer"]);
-		employeeListener = await setMode("employee", pairs["employee"]);
-		const currentState = await productState.getValues();
-		console.log("Current Spotify mode state:", currentState);
+		window.appDevListener = await setMode("app-developer", pairs["app-developer"]);
+		window.employeeListener = await setMode("employee", pairs["employee"]);
 	}
 
 	/*
@@ -628,22 +568,30 @@
 				isLarge: true,
 			});
 
-			const headerContainer = createElement("div", "main-trackCreditsModal-btnContainer")
-			const closeButton = document.querySelector(".main-trackCreditsModal-closeBtn");
 			const header = document.querySelector(".main-trackCreditsModal-header");
-			const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-			svgElement.setAttribute("width", "18");
-			svgElement.setAttribute("height", "18");
-			svgElement.setAttribute("viewBox", "0 0 32 32");
-			svgElement.setAttribute("fill", "currentColor");
-			svgElement.innerHTML = `<path d="M16 0c8.84 0 16 7.16 16 16a16.026 16.026 90 01-10.9 15.18c-.8.16-1.1-.34-1.1-.76 0-.54.02-2.26.02-4.4 0-1.5-.5-2.46-1.08-2.96 3.56-.4 7.3-1.76 7.3-7.9 0-1.76-.62-3.18-1.64-4.3.16-.4.72-2.04-.16-4.24 0 0-1.34-.44-4.4 1.64-1.28-.36-2.64-.54-4-.54-1.36 0-2.72.18-4 .54-3.06-2.06-4.4-1.64-4.4-1.64-.88 2.2-.32 3.84-.16 4.24-1.02 1.12-1.64 2.56-1.64 4.3 0 6.12 3.72 7.5 7.28 7.9-.46.4-.88 1.1-1.02 2.14-.92.42-3.22 1.1-4.66-1.32-.3-.48-1.2-1.66-2.46-1.64-1.34.02-.54.76.02 1.06.68.38 1.46 1.8 1.64 2.26.32.9 1.36 2.62 5.38 1.88 0 1.34.02 2.6.02 2.98 0 .42-.3.9-1.1.76A15.99 15.99 90 010 16c0-8.84 7.16-16 16-16Z"></path>`;
-			const button = createButton("main-trackCreditsModal-closeBtn", "", () =>
-				window.open("https://github.com/veryboringhwl/spicetify")
+			const closeButton = document.querySelector(".main-trackCreditsModal-closeBtn");
+			const buttonContainer = document.createElement("div");
+			buttonContainer.className = "main-trackCreditsModal-BtnContainer";
+
+			const githubButton = Spicetify.React.createElement(
+				"button",
+				{
+					className: "main-trackCreditsModal-githubBtn",
+					onClick: () => window.open("https://github.com/veryboringhwl/spicetify"),
+				},
+				Spicetify.React.createElement("svg", {
+					width: "18",
+					height: "18",
+					viewBox: "0 0 96 96",
+					fill: "currentColor",
+					dangerouslySetInnerHTML: {
+						__html: `<path d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"></path>`,
+					},
+				})
 			);
-			button.appendChild(svgElement);
-			headerContainer.appendChild(button);
-			headerContainer.appendChild(closeButton);
-			header.appendChild(headerContainer);
+			Spicetify.ReactDOM.render(githubButton, buttonContainer);
+			buttonContainer.appendChild(closeButton);
+			header.appendChild(buttonContainer);
 		},
 		false,
 		true
